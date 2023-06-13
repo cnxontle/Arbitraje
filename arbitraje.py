0b6b1fe325b4accff0e9111b0b249137e7afd546
cnxontle <cnxontle@gmail.com>
2023-06-13 14:59:24 -0600

    se agrega el segundo gap

--- arbitraje.py
+++ arbitraje.py
@@ -19,7 +19,7 @@ for moneda in monedas:
     for exchange in exchanges:
         exchange_name = exchange['market']['name']
         pair = exchange['target']
-        exchange_pair = exchange_name + pair
+        exchange_pair = exchange_name +"/"+ pair
         price = exchange['converted_last']['usd']
         bid_ask_spread = exchange['bid_ask_spread_percentage']
         trust_score = exchange['trust_score']
@@ -55,18 +55,21 @@ for moneda in monedas:
         exchange_par1 = row["Exchange_Par1"]
         exchange_par2 = row["Exchange_Par2"]
         
-
         for moneda2 in monedas:
             if moneda2 != moneda:
                 #buscar coincidencias
-                existe_exchange1_par1 = data[moneda2].loc[data[moneda2]["Exchange_Par"] == exchange_par1].index
-                if not existe_exchange1_par1.empty:
-                    print("si existe")
-                    existe_exchange2_par2 = data[moneda2].loc[(data[moneda2]["Exchange_Par"] == exchange_par2)].index[0]
+                indice_exchange1_par1 = data[moneda2].loc[data[moneda2]["Exchange_Par"] == exchange_par1].index
+                if indice_exchange1_par1 != 0:
+                    indice_exchange2_par2 = data[moneda2].loc[(data[moneda2]["Exchange_Par"] == exchange_par2)].index
+                    if indice_exchange2_par2 != 0:
+                        intercambio= moneda+"/"+moneda2
 
-                    if not existe_exchange2_par2.empty:
-                        
-                        print(x)
+                        valor_precio1 = data[moneda2].at[indice_exchange1_par1[0], 'Precio (USD)']
+                        valor_precio2 = data[moneda2].at[indice_exchange2_par2[0], 'Precio (USD)']
+                        gap2 = ((valor_precio1 - valor_precio2)/valor_precio1)*100
+
+                        mined.append([intercambio, exchange_par1, exchange_par2, gap, gap2])
+                        print(x, intercambio, exchange_par1, exchange_par2, gap, gap2)
                         x+=1
 
         #consolidar tablas y buscar gap inverso
